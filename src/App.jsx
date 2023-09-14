4591618b626f14fde888189ac6d036a3fccbe166
oladee <deemajor230600@gmail.com>
2023-09-14 01:55:37 +0100

    implementing movie detail section

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import Homepage from "./Homepage";
 import React from "react";
-import { useState, useEffect } from "react";
-import Landing from "./components/Landing";
-import Featured from "./components/Featured";
-import axios from "axios";
-import LoadingAnim from "./components/LoadingAnim";
-import Footer from "./components/Footer";
+import MovieDetail from "./components/MovieDetail";
+
 
 const App = () => {
-  const [data, setData] = useState([
-    {
-      title: " ",
-      overview: " ",
-      poster_path: " ",
-      vote_average: " ",
-      id: " ",
-      vote_count: " ",
-    },
-  ]);
-
-  const [loading, setLoading] = useState(false);
-
-
-  useEffect(() => {
-    async function getData() {
-      try {
-        setLoading(true);
-        const value = await axios.get(
-          `https://api.themoviedb.org/3/movie/top_rated`,{
-            params:{
-              api_key: import.meta.env.VITE_SOME_KEY,
-              language: 'en-US',
-              page: 1
-            }
-          });
-        setData(value.data.results);
-        setTimeout(()=>{
-          setLoading(false)
-        },2000)
-        
-      } catch (error) {
-        setLoading(false);
-        console.log(error);
-      }
-    }
-    getData();
-  },[]);
-
-
-  return (
+  return(
     <>
-      {loading ? (
-        <div >
-          <LoadingAnim/>
-        </div>
-      ) : (
-        <>
-          <Landing data={data} setLoading={setLoading} setData={setData}/>
-          <Featured data={data}/>
-          <Footer/>
-        </>
-      )}
-      {loading ? (<div><LoadingAnim/></div>):(
-        <></>
-      )}
+    <Routes>
+      <Route path="/" element={<Homepage/>}/>
+      <Route path="/movie/:id" element={<MovieDetail/>}/>
+    </Routes>
     </>
-  );
+  )
 };
 
 export default App;
