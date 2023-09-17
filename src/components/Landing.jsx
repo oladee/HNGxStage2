1dbc4cc6d6544b6d6ce3c7e18306e72dd5dbb8d6
oladee <deemajor230600@gmail.com>
2023-09-17 23:04:06 +0100

    restructure

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -39,7 +39,8 @@ const search = async (queries) =>{
 
   const [inputValue, SetInputValue] = useState('')
   
-  function handleSearch(){
+  function handleSearch(e){
+    e.preventDefault()
     search(inputValue)
   }
 
